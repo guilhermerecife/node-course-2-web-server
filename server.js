dec030c340830218be6acb97525dc54136546bc0
guilhermerecife <guilherme_liver@hotmail.com>
2018-12-09 18:43:58 -0300

    Inicial Commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const serverPort = 3000;
+
+var app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}\n`;
+    fs.appendFile('server.log', log, (e) => {
+        if(e) console.log('Unable to log request!');
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+hbs.registerHelper('screamIt', (textMessage) => {
+    return textMessage.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website :)',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.listen(serverPort, () => {  
+    console.log(`Server is listening on port ${serverPort}`);
+});
