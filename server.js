ab18df467dfd86782f193e08a16e29c6abb08d33
guilhermerecife <guilherme_liver@hotmail.com>
2018-12-09 20:01:12 -0300

    Add Projects page

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.render('maintenance.hbs');
-});
+});*/
 
 app.use(express.static(`${__dirname}/public`));
 
@@ -33,15 +33,19 @@ hbs.registerHelper('screamIt', (textMessage) => {
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my website :)',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to my website :)'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
     });
 });
 
